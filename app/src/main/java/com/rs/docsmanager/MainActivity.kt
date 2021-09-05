c89d6639ad96361d5dbb1da97589055c3cfe3391
ReubenSunish <reuben.sunish@gmail.com>
2021-09-05 18:53:43 +0530

    main

--- app/src/main/java/com/rs/docsmanager/MainActivity.kt
+++ app/src/main/java/com/rs/docsmanager/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rs.docsmanager
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                doMySearch(query)
+            }
+        }
     }
+
+
+    }
+
+
+
+
 }
