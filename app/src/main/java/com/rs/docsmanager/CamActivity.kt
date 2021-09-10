9a45419e18b5ccb8d8421271b91329a6eb5e0dd9
InfernalSpark <shriramkiran05@gmail.com>
2021-09-10 12:31:15 +0530

    working cam

--- app/src/main/java/com/rs/docsmanager/CamActivity.kt
+++ app/src/main/java/com/rs/docsmanager/CamActivity.kt
@@ -2,12 +2,25 @@ package com.rs.docsmanager
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_cam.*
 import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CamActivity : AppCompatActivity() {
     private var imageCapture: ImageCapture? = null
@@ -17,10 +30,55 @@ class CamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cam)
+
+        if(allPermissionsGranted()){
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        camera_capture_button.setOnClickListener{takePhoto()}
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    private fun takePhoto(){}
-    private fun startCamera(){}
+    private fun takePhoto(){
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis()) + ".jpg")
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback{
+            override fun onError(exception: ImageCaptureException) {
+                Log.e("le_tag", "FAIL AGGITE ${exception.message}", exception)
+            }
+
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Foto captured and saved in $savedUri"
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                Log.d("LE_TAG", msg)
+            }
+        })
+    }
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also { it.setSurfaceProvider(viewFinder.surfaceProvider) }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (exc : Exception){
+                Log.e("CAM", "FAIL AGGITTE", exc)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
@@ -44,5 +102,21 @@ class CamActivity : AppCompatActivity() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if (allPermissionsGranted()){
+                startCamera()
+            } else {
+                Toast.makeText(this, "No perms", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/rs/docsmanager/MainActivity.kt
+++ app/src/main/java/com/rs/docsmanager/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.rs.docsmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openCam(view:View){
+        val intent = Intent(this, CamActivity::class.java).apply {
+            putExtra("LE_TAG", "le_message")
+        }
+        startActivity(intent)
     }
 }
